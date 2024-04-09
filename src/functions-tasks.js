8b3a7a4ae77dd6f11b0fcd112215d26d8bde23fb
playoffthecuff <jmobile@bk.ru>
2024-04-09 10:22:23 +0300

    feat: add 3rd task solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,8 +50,10 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const result = [];
+  funcs.forEach((_, index) => result.push(index));
+  return result;
 }
 
 /**
